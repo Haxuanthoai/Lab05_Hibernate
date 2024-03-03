69aed1860a98e1a9b5a4c9cf5c2148d8a9c29ea1
Haxuanthoai <haxuanthoai543@gmail.com>
2024-03-03 22:39:23 +0700

    Lab05

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Product(
+MaSP nvarchar (5) NOT NULL PRIMARY KEY,
+TenSanPham nvarchar (500) NULL,
+AnhSanPham nvarchar (500) NULL,
+SoLuong int NULL,
+DonGia float Null,
+TrangThai bit NULL
+)
+GO
